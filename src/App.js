fe57e052d8a045f81e02e67e006586fdf831b461
KunalAnkur <mrankur801@gmail.com>
2023-01-02 23:12:46 +0530

    checkpoint2 finish

--- src/App.js
+++ src/App.js
@@ -3,28 +3,58 @@ import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  const [title, setTitle] = useState("");
   const [note, setNote] = useState("");
+  const [noteDocument, setNoteDocument] = useState({});
   const [noteList, setNoteList] = useState([]);
+
+  const [show, setShow] = useState(false);
+
   function onSubmit() {
-    console.log(note);
-    setNoteList((prev) => [...prev, note]);
+    const newDocument = {
+      title: title,
+      note: note,
+    };
+    setNoteDocument(newDocument);
+    const array = [...noteList, newDocument];
+    setNoteList(array);
   }
-  function onType(e) {
-    setNote(e.target.value);
+  function onTypeTitle(e) {
+    setTitle(e.target.value);
   }
 
+  function onTypeNote(e) {
+    setNote(e.target.value);
+  }
+  function onStartCreatingNote() {
+        setShow((prev) => !prev);
+  }
   return (
     <div className="App">
       <div className="input_field">
-        <input onChange={onType} type="text" placeholder="Write a note" />
-        <button onClick={onSubmit}>Submit</button>
+        {show ? (
+          <div className="create_note_container">
+            <input onChange={onTypeTitle} type={"text"} placeholder="Title" />
+            <input
+              onChange={onTypeNote}
+              type={"text"}
+              placeholder="Write you notes"
+            />
+            <div className="button_container">
+              <button onClick={onSubmit}>Submit</button>
+            </div>
+          </div>
+        ) : (
+          <div onClick={onStartCreatingNote} className="take_a_note">
+            <p>Take a note</p>
+          </div>
+        )}
       </div>
       <div className="notelist_container">
-        {noteList.map((ele, index) => (
-        <Card key={index} note={ele}/>
-      ))}
+        {noteList.map((ele,index) => 
+           <Card key={index} title={ele.title} note={ele.note} />
+        )}
       </div>
-      
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import "./Card.css"
 function Card(props) {
+  console.log(props)
   return (
     <div className="card">
+      <h4>{props.title}</h4>
       <p>{props.note}</p>
     </div>
   );
